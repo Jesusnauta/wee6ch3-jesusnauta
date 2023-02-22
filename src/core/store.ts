3d75f15eebc0dac4df0ed189a62fed1c0816f09c
Jesus <jesus.cassano.2006@gmail.com>
2023-02-23 00:04:39 +0100

    ✅ Add store files

---
+++ src/core/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { phoneReducer } from "../feature/reducer/phone.reducer";
+
+export const store = configureStore({
+  reducer: {
+    phone: phoneReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
