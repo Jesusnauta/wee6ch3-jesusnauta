f0d85c202a243b4df803de8f2e790142c1abc718
Jesus <jesus.cassano.2006@gmail.com>
2023-02-23 07:53:44 +0100

    ✅ App files changes

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,12 +1,15 @@
+import { Display } from "../../../feature/components/display/display";
+import { Keyboard } from "../../../feature/components/keyboard/keyboard";
+import { Actions } from "../../../feature/components/actions/actions";
 import React from "react";
-import { HomePage } from "../../../feature/page/home";
-
 export function App() {
   return (
     <div className="container">
-      <HomePage></HomePage>
+      <Display></Display>
+      <main className="phone">
+        <Keyboard></Keyboard>
+        <Actions></Actions>
+      </main>
     </div>
   );
 }
-
-export default App;
