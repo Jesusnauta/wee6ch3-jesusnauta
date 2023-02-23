764aa6e536d209bff2fda959633a430cfaaeba94
Jesus <jesus.cassano.2006@gmail.com>
2023-02-23 07:54:01 +0100

    ✅ Add chnges index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
-import { store } from "./app/store";
+import { App } from "./core/components/app/App";
+import { store } from "./core/store";
 import "./index.css";
 
 const container = document.getElementById("root")!;
@@ -11,11 +11,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App></App>
+      <App />
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
