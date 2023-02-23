aecafefcac15ae57fff44dd5802fb1511a7b27a1
Jesus <jesus.cassano.2006@gmail.com>
2023-02-23 07:54:37 +0100

    ✅ Add changes actions file

--- src/feature/components/actions/actions.tsx
+++ src/feature/components/actions/actions.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+
 import React from "react";
-import { Display } from "../display/display";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../core/store";
 
 export function Actions() {
+  const numbers = useSelector((state: RootState) => state.phone);
   return (
     <div className="actions">
-      <Display></Display>
+      <span className="number">{numbers}</span>
       <a href="#" className="call">
         Call
       </a>
+
       <a href="#" className="hang active">
         Hang
       </a>
